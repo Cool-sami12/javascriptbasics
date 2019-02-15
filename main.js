9c098104bdea5ad5e0c92a825432e607ae43011f
Samuel <coolgen4@outlook.com>
2019-02-15 14:06:21 +0100

    changes to the main.js file

---
+++ main.js
@@ -0,0 +1,12 @@
+// inline comment or /*multi line   */
+var number = 3;//declaring variable and assiging value
+sum = number + 10;
+console.log (sum);
+var n = 11;
+n++;//increment
+n--;//decrement
+ var remainder;
+ remainder = 11 % 3;
+ console.log(remainder);
+ var myArray =[["Bulls", 23], ["White " 45]];//nested myArray
+ myArray.push(["cool", "sami"]);//added an array to the original array 
